ceb297d3ad126be7daa5707f9ea2384cfafee731
IngTelemundoMEX <ing.mex.tlmd@gmail.com>
2019-03-13 03:32:37 -0600

    Push 1
    Se trabaja en front end newcesita haecerse un crud en el back end

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+import { PickerController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+
+export class Tab2Page {
+
+  datos={}
+
+}
